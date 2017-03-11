35cf3c2e3e6227fb82a6aa221cc344650dd5b383
Michael Allan <michael.allan@anz.com>
2017-03-12 10:33:09 +1100

    Remove local state, and refactor Stream to stateless functional component

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react'
 
-class Stream extends Component {
-
-  constructor() {
-    super()
-    this.state = {}
-  }
-
-  render() {
-    const { tracks = [] } = this.props
-
-    return <div>
-      {
-        tracks.map((track, key) => {
-          return <div key={ `track-${key}` } className="track">
-            { track.title }
-            <button tpye="button" onClick={() => this.setState({ [key] : !this.state[key] })}>
-              { this.state[key] ? 'Dislike' : 'Like' }
-            </button>
-          </div>
-        })
-      }
-    </div>
-  }
+function Stream({ tracks = [] }) {
+  return <div>
+    {
+      tracks.map((track, key) => {
+        return <div key={ `track-${key}` } className="track">{ track.title }</div>
+      })
+    }
+  </div>
 }
 
 export default Stream
