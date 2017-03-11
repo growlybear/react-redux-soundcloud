d2f75b83f3e20ac043e9af320d13804bef1cce2d
Michael Allan <michael.allan@anz.com>
2017-03-12 10:47:59 +1100

    Setup component testing with mocha, chai and enzyme

---
+++ test/setup.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { expect } from 'chai'
+import jsdom from 'jsdom'
+
+const doc = jsdom.jsdom(
+  `<!DOCTYPE html>
+    <html></html>
+    <body></body>
+  </html>`
+)
+const win = doc.defaultView
+
+global.document = doc
+global.window = win
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key]
+  }
+})
+
+global.React = React
+global.expect = expect
