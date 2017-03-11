3561c8f9c77acc3ea5157dcb73c1557a0d8b769a
Michael Allan <michael.allan@anz.com>
2017-03-11 18:41:40 +1100

    Track state of Like buttons within Stream component

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 class Stream extends Component {
+
+  constructor() {
+    super()
+    this.state = {}
+  }
+
   render() {
     const { tracks = [] } = this.props
 
     return <div>
       {
         tracks.map((track, key) => {
-          return <div key={ `track-${key}` } className="track">{ track.title }</div>
+          return <div key={ `track-${key}` } className="track">
+            { track.title }
+            <button tpye="button" onClick={() => this.setState({ [key] : !this.state[key] })}>
+              { this.state[key] ? 'Dislike' : 'Like' }
+            </button>
+          </div>
         })
       }
     </div>
